a09b67238850d167e3b1110ceb6ae766797a09e6
andrey podgorniy <andrjuxa201185@gmai.com>
2019-02-02 22:45:46 +0200

    finish add class play

--- script/play.js
+++ script/play.js
@@ -5,7 +5,7 @@ class Play {
         this.raf;
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
-        this.countOfSquare = 100;
+        this.countOfSquare = 10;
         this.speed = 4;
         this.score = 0;
         this.isAnimated = false;
@@ -17,19 +17,7 @@ class Play {
         this.ctx.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight); 
         for (let i = 0; i < this.countOfSquare; i++) {
             if (this.square[i]) {
-                if(this.square[i-1]){
-                    this.square[i].drow(this.ctx, this.square[i-1].positionX, this.square[i-1].positionY);
-                } 
-                if(this.square[i-2]){
-                    this.square[i].drow(this.ctx, this.square[i-2].positionX, this.square[i-2].positionY);
-                } 
-                if(this.square[i-3]){
-                    this.square[i].drow(this.ctx, this.square[i-3].positionX, this.square[i-3].positionY);
-                } 
-                else {
-                    this.square[i].drow(this.ctx, this.square[i].positionX, this.square[i].positionY);
-                }
-
+                this.square[i].drow(this.ctx);
                 this.square[i].move();
                 this.square[i].checkPosition(this.canvas);
             }
@@ -64,7 +52,7 @@ class Play {
         let y = e.offsetY;
         for (let i = 0; i < this.countOfSquare; i++) {
             if (this.square[i]){
-                if (x > this.square[i].positionX && x < (this.square[i].positionX + 20) && y > this.square[i].positionY && y < (this.square[i].positionY + 20)) {
+                if (x > this.square[i].positionX && x < (this.square[i].positionX + 30) && y > this.square[i].positionY && y < (this.square[i].positionY + 30)) {
                     this.square[i].setParam(this.speed);
                     return true;
                 }
--- script/square.js
+++ script/square.js
@@ -2,45 +2,30 @@ class Square {
     constructor () {
       this.positionX = 0;
       this.positionY = 0;
-      this.speedY = 3;
-      this.speedX = 3;
+      this.speedY = 1;
       this.colorSquare = "red";
     }
-    setParam(speedY = 3, speedX = 1){
-      this.speedY = Math.random() * speedY + 0.5;
-      this.speedX = Math.random() * speedX;
+
+    setParam(speedY = 1){
+      this.speedY = Math.random() * speedY;
       this.positionY = 0;
-      this.positionX = Math.random() * (640 - 20) ;
+      this.positionX = Math.random() * (1040 - 30) ;
       this.colorSquare = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
     }
-    drow(ctx, x, y){
-      ctx.fillStyle = "grey";
-      ctx.beginPath();
-      ctx.moveTo(x + 10, y + 10);
-      ctx.lineTo(this.positionX+10, this.positionY+10);
-      ctx.lineTo(this.positionX+11, this.positionY+11);
-      ctx.lineTo(x + 11, y + 11);
-      ctx.fill();
 
+    drow(ctx){
       ctx.fillStyle = this.colorSquare;
-      ctx.fillRect(this.positionX, this.positionY, 20, 20);
+      ctx.fillRect(this.positionX, this.positionY, 30, 30);
     }
-    move(){
-      this.positionY += this.speedY; 
-      this.positionX += this.speedX; 
-    }
-    checkPosition(elem){
-      let canvasHeight = elem.clientHeight;
-      let canvasWidth = elem.clientWidth;
 
-      if(this.positionY >= canvasHeight || this.positionY < 0) {
-        // this.setParam(this.speed);
-        this.speedY = -this.speedY;
-      }
-      if(this.positionX >= canvasWidth || this.positionX < 0) {
-        this.speedX = -this.speedX;
-      }
-      
+    move(){
+      this.positionY += this.speedY;  
+    }
+
+    checkPosition(elem){
+      if(this.positionY >= elem.clientHeight ) {
+        this.setParam(this.speedY);
+      }     
     }
   }
 
